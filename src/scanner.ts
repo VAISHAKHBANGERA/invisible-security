d2f8ecf4316a765762baef53ea2b2f3d5caea9ed
VAISHAKHBANGERA <nayanaganesh601@gmail.com>
2026-04-18 02:07:31 +0530

    Integrated Member 1, 3, and 4  logic with stable placeholder for scanner

---
+++ src/scanner.ts
@@ -0,0 +1,5 @@
+import * as vscode from 'vscode';
+
+export async function scanPackageJson() {
+    vscode.window.showInformationMessage("Scanner is currently empty. Waiting for Member 3's code!");
+}
